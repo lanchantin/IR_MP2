0b107c784b5f161e66532182706e33e69d856b3f
Jack Lanchantin <jacklanchantin@gmail.com>
2014-10-06 18:38:13 -0400

    First IR2 Commit

---
+++ src/edu/illinois/cs/index/similarities/TFIDFDotProduct.java
@@ -0,0 +1,28 @@
+package edu.illinois.cs.index.similarities;
+
+import org.apache.lucene.search.similarities.BasicStats;
+import org.apache.lucene.search.similarities.SimilarityBase;
+
+public class TFIDFDotProduct extends SimilarityBase {
+    /**
+     * Returns a score for a single term in the document.
+     *
+     * @param stats
+     *            Provides access to corpus-level statistics
+     * @param termFreq
+     * @param docLength
+     */
+    @Override
+    protected float score(BasicStats stats, float termFreq, float docLength) {
+    	//getAvgFieldLength(): average document length
+    	//getNumberOfDocuments(): total number of documents in the index
+    	//getDocFreq(): the number of documents the current term appears in
+    	float r = (float) ((termFreq)*(Math.log(stats.getNumberOfDocuments()+1)/stats.getDocFreq()));
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        return "TF-IDF Dot Product";
+    }
+}
